e3d24532a437d02b3ba68b995c2b44faa8c15a44
yushi <yushihuang@gmail.com>
2018-05-06 02:26:25 +0800

    able to restore contract status

--- core/jvm/jvm.go
+++ core/jvm/jvm.go
@@ -1,7 +1,7 @@
 package jvm
 
 import "fmt"
-//import "strings"
+import "strings"
 import "encoding/binary"
 import "math"
 
@@ -61,6 +61,7 @@ func (self *JVM) ExecContract(contractCode []byte, input []byte, contractAddr co
      methodName := string(input) //todo
      method := class.GetInstanceMethod(methodName, "()V") //todo
      obj := class.NewObject()
+     reincarnateObject(obj, contractAddr, stateDB)
 
      frame := self.mainThread.NewFrame(method)
      self.mainThread.PushFrame(frame)
@@ -73,6 +74,13 @@ func (self *JVM) ExecContract(contractCode []byte, input []byte, contractAddr co
 }
 
 //todo optimize array storage
+const (
+    TypeEntry = 0 
+    ArrayLengthEntry = 1
+    NullSignal = 2
+    RefEntry = 3
+    SlotIndexOffset = RefEntry+1
+)
 
 func persistObjectGraph(rootObj *heap.Object, contractAddr common.Address, stateDB interf.StateDB) {
      persisted := make(map[*heap.Object][]uint)
@@ -80,44 +88,26 @@ func persistObjectGraph(rootObj *heap.Object, contractAddr common.Address, state
      persist(rootObj, []uint{}, false, contractAddr, stateDB, persisted)     
 }
 
-func write (path []uint, hash common.Hash, contractAddr common.Address, stateDB interf.StateDB) {
-     fmt.Printf("---writing %v = %v\n", path, hash)
-     stateDB.SetState(contractAddr, pathToHash(path), hash)
- }
-
-func writeBytes (path []uint, bs []byte, contractAddr common.Address, stateDB interf.StateDB) {
-     var h common.Hash
-     binary.LittleEndian.PutUint16(h[0:2], uint16(len(bs)))
-     size := min(len(bs), common.HashLength-2)
-     copy(h[2:], bs[0:size])
-     write(path, h, contractAddr, stateDB)
-     bs = bs[size:]
-     var i uint = 0
-     for len(bs)>0 {
-         size = min(len(bs), common.HashLength)
-         var h common.Hash
-         copy(h[:], bs[0:size])
-         write(append(path, i), h, contractAddr, stateDB)
-         bs = bs[size:]
-         i += 1 
-     } 
- }
-
 func persist (obj *heap.Object, pathPrefix []uint, persistType bool, contractAddr common.Address, stateDB interf.StateDB, persisted map[*heap.Object][]uint) {
+     if obj == nil {
+         writeBytes(append(pathPrefix, NullSignal), []byte{1}, contractAddr, stateDB)
+         return
+     }
      if path, ok := persisted[obj]; ok {
-         writeBytes(append(pathPrefix, Ref), pathToBytes(path), contractAddr, stateDB)
+         writeBytes(append(pathPrefix, RefEntry), pathToBytes(path), contractAddr, stateDB)
          return
      }
      persisted[obj] = pathPrefix
 
      if persistType {
-         writeBytes(append(pathPrefix, TypeOrLength), []byte(obj.Class().Name()), contractAddr, stateDB)
+         writeBytes(append(pathPrefix, TypeEntry), []byte(obj.Class().Name()), contractAddr, stateDB)
      }
 
      if obj.Class().IsArray() {
-         write(append(pathPrefix, TypeOrLength), intToHash(int(obj.ArrayLength())), contractAddr, stateDB)
-         for i:=0; i<int(obj.ArrayLength()); i++ {
-             path := append(pathPrefix, uint(i+1)) // 0 is type or length
+         write(append(pathPrefix, ArrayLengthEntry), intToHash(int(obj.ArrayLength())), contractAddr, stateDB)
+         var i uint
+         for i=0; i<uint(obj.ArrayLength()); i++ {
+             path := append(pathPrefix, i+SlotIndexOffset) 
 
              switch obj.Data().(type) {
              case []int8:
@@ -135,12 +125,11 @@ func persist (obj *heap.Object, pathPrefix []uint, persistType bool, contractAdd
              case []float64:
                  write(path, doubleToHash(obj.Data().([]float64)[i]), contractAddr, stateDB)
              case []*heap.Object:
-                 o := obj.Data().([]*heap.Object)[i]
-                 if o != nil {
-                     persist(o, path, o.Class().Name()!=obj.Class().Name()[1:], contractAddr, stateDB, persisted)
-                 }
+                 elem := obj.Data().([]*heap.Object)[i]
+                 //todo: better persist type judegment
+                 persist(elem, path, true, contractAddr, stateDB, persisted)
              default:
-                //todo
+                //todo should not happen
              }
          }
      } else {
@@ -153,9 +142,13 @@ func persist (obj *heap.Object, pathPrefix []uint, persistType bool, contractAdd
              descriptor := field.Descriptor()
              slotId := field.SlotId()
              
-             fmt.Printf("---persisting field %v (slot %v) %v for %v\n", field.Name(), slotId, descriptor, obj.Class().Name())
+             if (descriptor[0]=='L' || descriptor[0]=='[') && slots.GetRef(slotId)!=nil {
+                 fmt.Printf("---%v is persisting field %v (slot %v) type %v with %v\n", obj.Class().Name(), field.Name(), slotId, descriptor, slots.GetRef(slotId).Class().Name())
+             } else {
+                 fmt.Printf("---%v is persisting field %v (slot %v) type %v\n", obj.Class().Name(), field.Name(), slotId, descriptor)
+             }
 
-             path := append(pathPrefix, slotId+1) // 0 is type of length
+             path := append(pathPrefix, slotId+SlotIndexOffset) 
 
              //todo optimize for Ljava/lang/String;
 
@@ -170,14 +163,22 @@ func persist (obj *heap.Object, pathPrefix []uint, persistType bool, contractAdd
                  write(path, doubleToHash(slots.GetDouble(slotId)), contractAddr, stateDB)
              case 'L':
                  o := slots.GetRef(slotId)
-                 if o != nil {
-                     persist(o, path, ("L"+o.Class().Name()+";")!=descriptor, contractAddr, stateDB, persisted)
+                 var persistType bool
+                 if o == nil {
+                     persistType = false // actually not relevant
+                 } else {
+                     persistType = "L"+o.Class().Name()+";" != descriptor
                  }
+                 persist(o, path, persistType, contractAddr, stateDB, persisted)
              case '[':
                  o := slots.GetRef(slotId)
-                 if o != nil {
-                     persist(o, path, false, contractAddr, stateDB, persisted)
+                 var persistType bool
+                 if o == nil {
+                     persistType = false // actually not relevant
+                 } else {
+                     persistType = o.Class().Name() != descriptor
                  }
+                 persist(o, path, persistType, contractAddr, stateDB, persisted)
              default:
              // todo
              }
@@ -185,10 +186,157 @@ func persist (obj *heap.Object, pathPrefix []uint, persistType bool, contractAdd
      } // if array
 } 
 
-const (
-    TypeOrLength = 0
-    Ref = math.MaxUint16
-)
+func reincarnateObject(obj *heap.Object, contractAddr common.Address, stateDB interf.StateDB) {
+     pool := make(map[string]*heap.Object) // actually (path []uint] => *heap.Object
+     pool[""] = obj
+     reincarnate(obj, []uint{}, contractAddr, stateDB, pool)
+}
+
+func reincarnate(obj *heap.Object, pathPrefix []uint, contractAddr common.Address, stateDB interf.StateDB, pool map[string]*heap.Object) {
+     if obj.Class().IsArray() {
+         var i uint
+         for i=0; i<uint(obj.ArrayLength()); i++ {
+             path := append(pathPrefix, i+SlotIndexOffset)
+             switch obj.Data().(type) {
+             case []int8:
+                 v := hashToInt(read(path, contractAddr, stateDB))
+                 obj.Bytes()[i] = int8(v)
+             case []int16:
+                 v := hashToInt(read(path, contractAddr, stateDB))
+                 obj.Shorts()[i] = int16(v)
+             case []int32:
+                 v := hashToInt(read(path, contractAddr, stateDB))
+                 obj.Ints()[i] = int32(v)
+             case []int64:
+                 v := hashToInt(read(path, contractAddr, stateDB))
+                 obj.Longs()[i] = int64(v)
+             case []uint16:
+                 v := hashToInt(read(path, contractAddr, stateDB))
+                 obj.Chars()[i] = uint16(v)
+             case []float32:
+                 v := hashToFloat(read(path, contractAddr, stateDB))
+                 obj.Floats()[i] = v
+             case []float64:
+                 v := hashToDouble(read(path, contractAddr, stateDB))
+                 obj.Doubles()[i] = v
+             case []*heap.Object:
+                 elem := loadObjectOrArray(path, obj.Class().Loader(), obj.Class().Name()[1:len(obj.Class().Name())-1], contractAddr, stateDB, pool)
+                 obj.Refs()[i] = elem
+             default:
+                //todo should not happen
+             }
+         } // for array
+     } else {
+         slots := obj.Fields()
+
+         for _, field := range(obj.Class().Fields()) {
+             if field.IsStatic() {
+                 continue
+             }
+             descriptor := field.Descriptor()
+             slotId := field.SlotId()
+             
+             path := append(pathPrefix, slotId+SlotIndexOffset)
+
+             fmt.Printf("---loading field %v (slot %v) %v from %v\n", field.Name(), slotId, descriptor, path)
+             switch descriptor[0] {
+             case 'Z', 'B', 'C', 'S', 'I':
+                 bs := read(path, contractAddr, stateDB)
+                 v := hashToInt(bs)
+                 slots.SetInt(slotId, int32(v))
+             case 'F':
+                  bs := read(path, contractAddr, stateDB)
+                  v := hashToFloat(bs)
+                  slots.SetFloat(slotId, v)
+              case 'J':
+                  bs := read(path, contractAddr, stateDB)
+                  v := hashToInt(bs)
+                  slots.SetLong(slotId, int64(v))
+              case 'D':
+                  bs := read(path, contractAddr, stateDB)
+                  v := hashToDouble(bs)
+                  slots.SetDouble(slotId, v)
+              case 'L':
+                  slots.SetRef(slotId, loadObjectOrArray(path, obj.Class().Loader(), descriptor[1:len(descriptor)-1], contractAddr, stateDB, pool))
+              case '[':
+                  slots.SetRef(slotId, loadObjectOrArray(path, obj.Class().Loader(), descriptor, contractAddr, stateDB, pool))
+              } // switch
+          } // for fields
+     } // if array
+}
+
+func loadObjectOrArray(path []uint, classLoader *heap.ClassLoader, descriptor string, contractAddr common.Address, stateDB interf.StateDB, pool map[string]*heap.Object) *heap.Object {
+      isArray := descriptor[0]=='['
+      var bs []byte
+      if !allZeros(read(append(path, NullSignal), contractAddr, stateDB)) {
+          return nil
+      } else if bs = readBytes(append(path, RefEntry), contractAddr, stateDB); !allZeros(bs) {
+          return pool[pathToString(bytesToPath(bs))]
+      } else {
+          bs = readBytes(append(path, TypeEntry), contractAddr, stateDB)
+          var t string
+          if allZeros(bs) {
+              t = descriptor
+          } else {
+              t = string(bs)
+          }
+          class := classLoader.LoadClass(t)
+          var o *heap.Object
+          if isArray {
+              count := uint(hashToInt(read(append(path, ArrayLengthEntry), contractAddr, stateDB)))
+              o = class.NewArray(count)
+          } else {
+              o = class.NewObject()
+          }
+          pool[pathToString(path)] = o
+          reincarnate(o, path, contractAddr, stateDB, pool)
+          return o
+     }
+}
+
+func read(path []uint, contractAddr common.Address, stateDB interf.StateDB) (ret []byte) {
+     ret = stateDB.GetState(contractAddr, pathToHash(path)).Bytes()
+     fmt.Printf("----read %v = %v\n", path, ret)
+     return
+}
+
+func write(path []uint, hash common.Hash, contractAddr common.Address, stateDB interf.StateDB) {
+     fmt.Printf("---writing %v = %v\n", path, hash)
+     stateDB.SetState(contractAddr, pathToHash(path), hash)
+}
+
+func readBytes(path []uint, contractAddr common.Address, stateDB interf.StateDB) (ret []byte) {
+     bs := read(path, contractAddr, stateDB)
+     left := int(binary.LittleEndian.Uint16(bs[0:2]))
+     bs = bs[2:]
+     var i uint = 0
+     for left>0 {
+         l := min(left, len(bs))
+         ret = append(ret, bs[0:l]...)
+         left -= l
+         bs = read(append(path, i), contractAddr, stateDB)
+         i += 1
+     } 
+     return
+}
+
+func writeBytes(path []uint, bs []byte, contractAddr common.Address, stateDB interf.StateDB) {
+     var h common.Hash
+     binary.LittleEndian.PutUint16(h[0:2], uint16(len(bs)))
+     size := min(len(bs), common.HashLength-2)
+     copy(h[2:], bs[0:size])
+     write(path, h, contractAddr, stateDB)
+     bs = bs[size:]
+     var i uint = 0
+     for len(bs)>0 {
+         size = min(len(bs), common.HashLength)
+         var h common.Hash
+         copy(h[:], bs[0:size])
+         write(append(path, i), h, contractAddr, stateDB)
+         bs = bs[size:]
+         i += 1 
+     } 
+ }
 
 func intToHash(i int) common.Hash {
      bs := make([]byte, 8)
@@ -196,14 +344,22 @@ func intToHash(i int) common.Hash {
      return common.BytesToHash(bs)
 }
 
-func hashToInt(h common.Hash) int {
-     return int(binary.LittleEndian.Uint64(h.Bytes()[:common.HashLength-8]))
+func hashToInt(h []byte) int {
+     return int(binary.LittleEndian.Uint64(h[common.HashLength-8:]))
 }
 
 func floatToHash(f float32) common.Hash {
      bs := make([]byte, 4)
      binary.LittleEndian.PutUint32(bs, math.Float32bits(f))
      return common.BytesToHash(bs)     
+}
+
+func hashToFloat(h []byte) float32 {
+     return math.Float32frombits(binary.LittleEndian.Uint32(h[common.HashLength-4:]))
+}
+
+func hashToDouble(h []byte) float64 {
+     return math.Float64frombits(binary.LittleEndian.Uint64(h[common.HashLength-8:]))
 }
 
 func doubleToHash(d float64) common.Hash {
@@ -220,6 +376,17 @@ func pathToBytes(path []uint) []byte {
         b = append(b, bs[0:n]...)
      }
      return b
+}
+
+func bytesToPath(bs []byte) []uint {
+     path := []uint{}
+     for len(bs) > 0 {
+         //todo: handle exception
+         u, n := binary.Uvarint(bs)
+         path = append(path, uint(u))
+         bs = bs[n:]
+     }
+     return path
 }
 
 func pathToHash(path []uint) common.Hash {
@@ -240,4 +407,21 @@ func min(x, y int) int {
     } else {
         return y
     }
+}
+
+func allZeros(bs []byte) bool {
+     for _, b := range(bs) {
+         if b != 0 {
+             return false
+         }
+     }
+     return true
+}
+
+func pathToString(path []uint) string {
+     ss := make([]string, len(path))
+     for i, u := range(path) {
+         ss[i] = string(u)
+     }
+     return strings.Join(ss, ",")
 }
